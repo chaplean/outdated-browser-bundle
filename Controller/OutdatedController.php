ee88a88690650a363af2887eb7aa4ae592425107
Valentin Bontemps <valentin@chaplean.com>
2015-11-05 16:31:00 +0100

    add path for min js

--- Controller/OutdatedController.php
+++ Controller/OutdatedController.php
@@ -2,6 +2,7 @@
 
 namespace Chaplean\Bundle\OutdatedBrowserBundle\Controller;
 
+use GK;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,9 +16,12 @@ use Symfony\Component\HttpFoundation\Response;
 class OutdatedController extends Controller
 {
     /**
+     * @param boolean $uglify
+     *
      * @return Response
+     * @throws \Exception
      */
-    public function outdatedAction()
+    public function outdatedAction($uglify = false)
     {
         $script = $this->renderView('ChapleanOutdatedBrowserBundle::outdated.js.twig', array(
             'template'   => $this->renderView($this->getParameter('chaplean_outdated_browser.template')),
@@ -25,6 +29,11 @@ class OutdatedController extends Controller
             'color' => $this->getParameter('chaplean_outdated_browser.color'),
             'lowerThan' => $this->getParameter('chaplean_outdated_browser.lower_than'),
         ));
+
+        if ($uglify) {
+            $packer = new GK\JavascriptPacker($script);
+            $script = $packer->pack();
+        }
 
         $response = new Response($script);
         $response->headers->set('Content-Type', 'text/javascript');
